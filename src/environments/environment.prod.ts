4e438a30788afc24005b4aad0553cbdbede32d45
Abdul Kader Itani <itani.aboudi@gmail.com>
2021-08-26 14:58:14 +0300

    [+] add project to github repo

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,7 @@
+import {Environment} from './interface';
+
+export const environment: Environment = {
+  production: true,
+  apiKey: 'AIzaSyD7YtlHlTpXwp2kkudRuSSuq1l4ialu72I',
+  fbDbUrl: 'https://pig-steam-default-rtdb.europe-west1.firebasedatabase.app'
+};
